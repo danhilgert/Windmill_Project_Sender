7d1b625813b4a670a2357d8cbaa8b3ee91a67c7f
danhilgert <hilge138@unm.edu>
2017-04-15 17:07:53 -0500

    went from array of 16 samples to 256.
    Added 2 function prototypes that are called from main.

--- Windmill_Sender/src/adc_app.h
+++ Windmill_Sender/src/adc_app.h
@@ -10,14 +10,15 @@
 #define ADC_APP_H_
 
  //! [result_buffer]
- #define ADC_SAMPLES 16
+ #define ADC_SAMPLES 256
  uint16_t adc_result_buffer[ADC_SAMPLES];
  //! [result_buffer]
 
 void adc_app_int(void);
 uint8_t adc_app_get_flag (void);
 void adc_app_reset_flag(void);
-
+void adc_app_service(void);
+uint16_t adc_app_get_value(void);
 
 
 #endif /* ADC_APP_H_ */
