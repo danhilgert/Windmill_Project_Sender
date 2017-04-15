fff1ad5053df543fc6f6b63482c96c91e2d2b713
danhilgert <hilge138@unm.edu>
2017-04-15 17:10:06 -0500

    Added timer code. used array of timers with getter and setter functions to allow for addition or subtraction of timers from app easily.

--- Windmill_Sender/src/timer_app.c
+++ Windmill_Sender/src/timer_app.c
@@ -4,3 +4,79 @@
  * Created: 4/14/2017 10:24:43 PM
  *  Author: dan_h_000
  */ 
+
+#include <asf.h>
+#include "timer_app.h"
+
+
+static void configure_tcc(void);
+static void configure_tcc_callbacks(void);
+static void tcc_callback_ms(struct tcc_module *const module_inst);
+
+struct tcc_module tcc_instance;
+
+volatile uint32_t msecs = 0;
+
+void timer_app_init(void)
+{
+configure_tcc();
+configure_tcc_callbacks();
+}
+
+static void tcc_callback_ms(struct tcc_module *const module_inst)
+{
+	msecs++;
+	port_pin_toggle_output_level(LED0_PIN);
+}
+
+
+
+static void configure_tcc(void)
+{
+	struct tcc_config config_tcc;
+
+	tcc_get_config_defaults(&config_tcc, TCC0);
+
+	config_tcc.counter.clock_source = GCLK_GENERATOR_0;
+	config_tcc.counter.clock_prescaler = TCC_CLOCK_PRESCALER_DIV1;
+	config_tcc.counter.period =   47619;
+
+	tcc_init(&tcc_instance, TCC0, &config_tcc);
+	tcc_enable(&tcc_instance);
+}
+
+static void configure_tcc_callbacks(void)
+{
+	tcc_register_callback(&tcc_instance, tcc_callback_ms,TCC_CALLBACK_OVERFLOW);
+	tcc_enable_callback(&tcc_instance, TCC_CALLBACK_OVERFLOW);
+}
+
+void timer_app_service (void)
+{
+	uint8_t i;
+	while(msecs)
+	{
+		for( i = 0; i < NUM_TIMERS; i++)
+		{
+			if(app_timers[i])
+			{
+				app_timers[i]--;
+			}	
+		}
+			
+
+		Disable_global_interrupt();
+		msecs--;
+		Enable_global_interrupt();
+	}	
+}
+
+void timer_app_set(TIMERS_t timer, TIMER_COUNTS_t count)
+{
+	app_timers[timer] = count;
+}
+
+uint32_t timer_app_get_timer(TIMERS_t timer)
+{
+	return app_timers[timer];
+}
--- Windmill_Sender/src/timer_app.h
+++ Windmill_Sender/src/timer_app.h
@@ -9,8 +9,28 @@
 #ifndef TIMER_APP_H_
 #define TIMER_APP_H_
 
+typedef enum{
+	HUNDRED_MS,
+	ONE_SEC,
+	TEN_SEC,
+	THIRTY_SEC,
+	NUM_TIMERS
 
+}TIMERS_t;
 
+typedef enum{
+	HUNDRED_MS_COUNT =		100,
+	ONE_SEC_COUNT =			1000,
+	TEN_SEC_COUNT =			10000,
+	THIRTY_SEC_COUNT =		30000
+}TIMER_COUNTS_t;
+
+uint32_t app_timers[NUM_TIMERS];
+
+void timer_app_init(void);
+void timer_app_set(TIMERS_t timer, TIMER_COUNTS_t count);
+void timer_app_service(void);
+uint32_t timer_app_get_timer(TIMERS_t timer);
 
 
 #endif /* TIMER_APP_H_ */
