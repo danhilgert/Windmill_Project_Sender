969dde7a293ad1edf237c4af7df67b33352c5e06
danhilgert <hilge138@unm.edu>
2017-04-14 23:00:33 -0500

    Added timer files. timer is not yet working.

---
+++ Windmill_Sender/src/timer_app.c
@@ -0,0 +1,6 @@
+/*
+ * timer_app.c
+ *
+ * Created: 4/14/2017 10:24:43 PM
+ *  Author: dan_h_000
+ */ 
---
+++ Windmill_Sender/src/timer_app.h
@@ -0,0 +1,16 @@
+/*
+ * timer_app.h
+ *
+ * Created: 4/14/2017 10:24:13 PM
+ *  Author: dan_h_000
+ */ 
+
+
+#ifndef TIMER_APP_H_
+#define TIMER_APP_H_
+
+
+
+
+
+#endif /* TIMER_APP_H_ */
